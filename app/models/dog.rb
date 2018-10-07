ae55f21e0045d6ce1aa4dc33e9bda6265b20d37f
Wendy Calderon <wendycalderon@gmail.com>
2018-10-07 20:23:31 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,8 +1,14 @@
-class Dogs 
-  attr_accessor :name, :breed, :age 
+class Dogs
+  attr_accessor :name, :breed, :age
+  @@all = []
   def initialize(name, breed, age)
-    @name = name 
+    @name = name
     @breed = breed
     @age = age
+    @@all << self
+  end
+
+  def self.all 
+    @@all
   end
 end
